c43dfb4cda17865e3229243d2e3ec5325c5aa8bb
Kolin Biju <kolinbijukulangara@gmail.com>
2026-04-15 21:00:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.querySelector('#theme-toggle i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+}
+
 function appendValue(value) {
     document.getElementById("result").value += value;
 }
@@ -6,7 +18,6 @@ function clearDisplay() {
     document.getElementById("result").value = "";
 }
 
-/* Erase one character */
 function erase() {
     let current = document.getElementById("result").value;
     document.getElementById("result").value = current.slice(0, -1);
